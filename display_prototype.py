542c767e7038fb1f9225c0b1564fea4e710c552a
nebhead <nebhead77@gmail.com>
2022-12-09 16:52:43 -0800

    Minor Visual Updates to Display Prototype
    Added a splash of color to the display prototype.

--- display_prototype.py
+++ display_prototype.py
@@ -22,12 +22,14 @@ class Display:
 		curses.wrapper(self._curses_main)
 		curses.curs_set(0)  # Invisible Cursor 
 		curses.start_color()  # Init Color
-		curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+		curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
 		curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
 		curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
-		curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_WHITE)
-		self.display_splash()
+		curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+		curses.init_pair(5, curses.COLOR_RED, curses.COLOR_BLACK)
+		curses.init_pair(6, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
+		self.display_splash()
 
 	def _curses_main(self, screen):
 		self.screen = screen
@@ -39,10 +41,12 @@ class Display:
 		self.screen.box()
 		title = f"| Mode: {status_data['mode']} |"
 		title_col = (num_cols // 2) - (len(title) // 2)
-		self.screen.addstr(0, title_col, title)
+		title_color = curses.color_pair(2)
+		self.screen.addstr(0, title_col, title, title_color)
 
 		''' Temp Info '''
 		line = 1
+		display_color = curses.color_pair(1)
 		for index, group in enumerate(in_data['probe_history']):
 			for item in in_data['probe_history'][group]:
 				if group != 'tr':
@@ -52,17 +56,17 @@ class Display:
 						display_text = f"{item} Setpoint: {in_data['primary_setpoint']} {self.units} Target: {in_data['notify_targets'][item]} {self.units}"
 					else: 
 						display_text = f"{item} Target: {in_data['notify_targets'][item]} {self.units}"
-					self.screen.addstr(line, (num_cols // 2), display_text)
+					self.screen.addstr(line, (num_cols // 2), display_text, display_color)
 					line += 1
 
 		''' Notification Info '''
 		line += 1
 		line_bak = line
-
+		display_color = curses.color_pair(4)
 		for index, item in enumerate(status_data['notify_data']):
 			if item['req']:
 				display_text = f" * {item['label']} Notify"
-				self.screen.addstr(line, (num_cols // 2), display_text)
+				self.screen.addstr(line, (num_cols // 2), display_text, display_color)
 				line += 1
 
 		''' Active Hardware Pins '''
@@ -78,8 +82,15 @@ class Display:
 
 		''' Show Pellet Level '''
 		line += 1
+		if status_data["hopper_level"] >= 70:
+			display_color = curses.color_pair(6)
+		elif status_data["hopper_level"] >= 25:
+			display_color = curses.color_pair(4)
+		else:
+			display_color = curses.color_pair(5)
+			
 		display_text = f'Pellet Level: {status_data["hopper_level"]}%'
-		self.screen.addstr(line, 3, display_text)
+		self.screen.addstr(line, 3, display_text, display_color)
 		
 		''' Show the screen '''
 		self.screen.refresh()
@@ -99,7 +110,8 @@ class Display:
 		self.screen.box()
 		title = '| PiFire Display |'
 		title_col = (num_cols // 2) - (len(title) // 2)
-		self.screen.addstr(0, title_col, title)
+		title_color = curses.color_pair(2)
+		self.screen.addstr(0, title_col, title, title_color)
 		splash_str_start = (num_cols // 2) - (len(splash_str[7]) // 2)
 		splash_color = curses.color_pair(2)
 		for line in range(0, len(splash_str)):
@@ -114,7 +126,8 @@ class Display:
 		self.screen.box()
 		title = '| PiFire Display |'
 		title_col = (num_cols // 2) - (len(title) // 2)
-		self.screen.addstr(0, title_col, title)
+		title_color = curses.color_pair(2)
+		self.screen.addstr(0, title_col, title, title_color)
 
 		self.screen.refresh()
 
