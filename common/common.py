539fe67601a53fa2e76d8de8eebd473fe5185408
nebhead <nebhead77@gmail.com>
2023-10-05 08:27:44 -0700

    Pin Flask to v2.3.3 in the Installer
    Pin Flask to v2.3.3 due to the recent update of Flask 3.0.0 which breaks compatibility with other packages.

--- common/common.py
+++ common/common.py
@@ -72,7 +72,7 @@ def default_settings():
 	settings = {}
 
 	settings['versions'] = {
-		'server' : "1.5.3",
+		'server' : "1.5.4",
 		'cookfile' : "1.5.0",  # Current cookfile format version
 		'recipe' : "1.0.0"  # Current recipe file format version
 	}
