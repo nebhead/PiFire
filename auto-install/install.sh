1e7b0d13737cebcec0a25eb646abc0a70bb4c04b
dogtreatfairy <98328354+dogtreatfairy@users.noreply.github.com>
2023-11-27 16:08:31 -0600

    Update install.sh
    Update to change all nebhead references to dogtreatfairy

--- auto-install/install.sh
+++ auto-install/install.sh
@@ -2,11 +2,11 @@
 
 # Automatic Installation Script
 # Many thanks to the PiVPN project (pivpn.io) for much of the inspiration for this script
-# Run from https://raw.githubusercontent.com/nebhead/pifire/master/auto-install/install.sh
+# Run from https://raw.githubusercontent.com/dogtreatfairy/pifire/master/auto-install/install.sh
 #
 # Install with this command (from your Pi):
 #
-# curl https://raw.githubusercontent.com/nebhead/pifire/master/auto-install/install.sh | bash
+# curl https://raw.githubusercontent.com/dogtreatfairy/pifire/master/auto-install/install.sh | bash
 #
 # NOTE: Pre-Requisites to run Raspi-Config first.  See README.md.
 
@@ -82,9 +82,9 @@ echo "**                                                                     **"
 echo "*************************************************************************"
 cd /usr/local/bin
 # Use a shallow clone to reduce download size
-$SUDO git clone --depth 1 https://github.com/nebhead/pifire
+$SUDO git clone --depth 1 https://github.com/dogtreatfairy/pifire
 # Replace the below command to fetch development branch
-#$SUDO git clone --depth 1 --branch development https://github.com/nebhead/pifire
+#$SUDO git clone --depth 1 --branch development https://github.com/dogtreatfairy/pifire
 
 # Setup Python VENV & Install Python dependencies
 clear
