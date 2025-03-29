1008633cbeec4964b4f4be2621782bc8b74cd655
markalston <mark@beernut.com>
2025-03-29 08:52:27 -0600

    lowered bus speed more

--- display/st7789e.py
+++ display/st7789e.py
@@ -45,8 +45,8 @@ class Display(DisplayBase):
 		rst_pin = self.dev_pins['display']['rst']
 		
 		#bus_speed_hz in [mhz * 1000000 for mhz in [0.5, 1, 2, 4, 8, 16, 20, 24, 28, 32, 36, 40, 44, 48, 50, 52]
-		self.serial = spi(gpio_DC=dc_pin, gpio_RST=rst_pin, bus_speed_hz=8000000)
-		self.device = st7789(self.serial, active_low=False, width=240, height=240, gpio_LIGHT=led_pin)
+		self.serial = spi(gpio_DC=dc_pin, gpio_RST=rst_pin)
+		self.device = st7789(self.serial, active_low=False, width=240, height=240, gpio_LIGHT=led_pin, bus_speed=4000000)
 
 		# Setup & Start Display Loop Thread 
 		self.device.backlight(True)
